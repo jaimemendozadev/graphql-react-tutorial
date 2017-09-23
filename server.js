1b08384ab8e7135714d64e0b1c656600d39d4c30
jaimemendozadev <jaimemendozadev@gmail.com>
2017-09-22 22:01:22 -0700

    Initial commit
    Setup server and dependencies.

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+const expressGraphQL = require('express-graphql'); //by convention, has to be typed GraphQL
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true, //only for dev environment
+
+}));
+
+app.listen(3000, ()=> {
+  console.log("Listening on port 3000");
+})
