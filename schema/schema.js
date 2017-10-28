944bea0a4ab7206703e1bad9514d9e56af3c4103
jaimemendozadev <jaimemendozadev@gmail.com>
2017-10-27 22:51:52 -0700

    Add association bet. User and Company

--- schema/schema.js
+++ schema/schema.js
@@ -8,13 +8,25 @@ const {
 } = graphql;
 
 const usersURL = `http://localhost:3000/users`;
+const companiesURL = `http://localhost:3000/companies`;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    description: {type: GraphQLString} 
+  }
+});
+
 
 const UserType = new GraphQLObjectType({
   name: 'User', //describes type we're defining
   fields: {
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
-    age: {type: GraphQLInt}
+    age: {type: GraphQLInt},
+    company: {type: CompanyType}  //treat associations bet. types as a field
   }
 });
 
@@ -25,7 +37,6 @@ const RootQuery = new GraphQLObjectType({
       type:UserType,
       args: {id: {type: GraphQLString}},
       resolve(parentValue, args) {
-        console.log(`${usersURL}/${args.id}`)
         return axios.get(`${usersURL}/${args.id}`).then( response => {
           return response.data;
         });        
